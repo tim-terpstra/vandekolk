35c41e3bdb616346d7b8b9f2ac83bb41074712c1
nrkruizinga <nrkruizinga@student.che.nl>
2021-10-08 13:32:13 +0200

    Login page v0.1

---
+++ kis20/kis20/Business/Login.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+
+namespace kis20.Business
+{
+    public class Login
+    {
+        public Login()
+        {
+        }
+    }
+}
---
+++ kis20/kis20/Pages/Login.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kis20.Pages
+{
+    public class LoginModel : PageModel
+    {
+        private readonly ILogger<LoginModel> _logger;
+
+        public LoginModel(ILogger<LoginModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+        public void OnYes()
+        {
+            Console.WriteLine("aaa");
+        }
+    }
+}
