d5cf541f3a683a4b262dc080e02a23a67419bd3e
nrkruizinga <nrkruizinga@student.che.nl>
2021-11-23 11:10:40 +0100

    aanpassingen front-end, index html toegevoegd capplan pagina toegevoegd projecten pagina toegevoegd

---
+++ kis20/kis20/Pages/Capplan.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kis20.Pages
+{
+    public class CapplanModel : PageModel
+    {
+        private readonly ILogger<CapplanModel> _logger;
+
+        public CapplanModel(ILogger<CapplanModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+        public void OnYes()
+        {
+            Console.WriteLine("aaa");
+        }
+    }
+}
---
+++ kis20/kis20/Pages/Projecten.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kis20.Pages
+{
+    public class ProjectenModel : PageModel
+    {
+        private readonly ILogger<ProjectenModel> _logger;
+
+        public ProjectenModel(ILogger<ProjectenModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+        public void OnYes()
+        {
+            Console.WriteLine("aaa");
+        }
+    }
+}
